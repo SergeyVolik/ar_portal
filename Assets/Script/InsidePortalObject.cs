00a0919c26ddfe7b2d99e9379249029fd2872381
SergeyVolik <volik.sergey112@gmail.com>
2024-05-09 11:52:34 +0300

    :sparkles: portal updated

--- Assets/Script/InsidePortalObject.cs
+++ Assets/Script/InsidePortalObject.cs
@@ -5,11 +5,21 @@ namespace Prototype
 {
     public class InsidePortalObject : MonoBehaviour
     {
-        private Renderer[] m_rendrers;
+        private Renderer[] Rendrers
+        {
+            get
+            {
+                if (m_Rendrers == null || m_Rendrers.Length == 0)
+                    m_Rendrers = GetComponentsInChildren<Renderer>();
+                return m_Rendrers;
+            }
+        }
+
+        private Renderer[] m_Rendrers;
 
         private void Awake()
         {
-            m_rendrers = GetComponentsInChildren<Renderer>();
+            m_Rendrers = null;
             ShowOnlyInPortal();
         }
 
@@ -17,13 +27,19 @@ namespace Prototype
         {
             var stencilTest = fullRender ? CompareFunction.Equal : CompareFunction.NotEqual;
 
-            foreach (var item in m_rendrers)
+            foreach (var item in Rendrers)
             {
                 foreach (var material in item.sharedMaterials)
                 {
                     material.SetInt("_StencilComp", (int)stencilTest);
                 }
             }
+        }
+
+        private void OnValidate()
+        {
+            if (!Application.isPlaying)
+                ShowAll();
         }
 
         public void ShowOnlyInPortal()
